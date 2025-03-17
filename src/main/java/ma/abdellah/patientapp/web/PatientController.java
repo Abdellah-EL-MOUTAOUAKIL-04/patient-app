da29bec88e3939facb318818918f728781c400f5
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-17 10:42:00 +0000

    feat: added pagination

--- src/main/java/ma/abdellah/patientapp/web/PatientController.java
+++ src/main/java/ma/abdellah/patientapp/web/PatientController.java
@@ -2,9 +2,12 @@ package ma.abdellah.patientapp.web;
 
 import ma.abdellah.patientapp.entities.Patient;
 import ma.abdellah.patientapp.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,12 @@ public class PatientController {
     }
     
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
+    public String index(Model model,@RequestParam(name = "page", defaultValue = "0") int page,
+    @RequestParam(name = "size", defaultValue = "5") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 }
